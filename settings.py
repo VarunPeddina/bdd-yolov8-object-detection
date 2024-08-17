8f02ee4fc89ace63138cf223cf405da162612b30
VarunPeddina <varun.peddina@gmail.com>
2024-08-17 12:08:53 +0530

    changed default images that was detected by our fine tuned model

--- settings.py
+++ settings.py
@@ -22,8 +22,10 @@ SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+DEFAULT_IMAGE = IMAGES_DIR / 'b1d10d08-c35503b8.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'b1d10d08-c35503b8_detected.jpg'
+# DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
+# DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
